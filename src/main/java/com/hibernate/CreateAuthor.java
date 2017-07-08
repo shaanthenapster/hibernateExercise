3209716d7cff73407b7bfc815367d51fb2237984
shaanthenapster <sanu.bhattacharya@tothenew.com>
2017-07-08 20:33:39 +0530

    question 13

--- src/main/java/com/hibernate/CreateAuthor.java
+++ src/main/java/com/hibernate/CreateAuthor.java
@@ -27,12 +27,19 @@ public class CreateAuthor {
         author1.getAddress().setLocation("Dwarka");
         author1.getAddress().setStreenNo("1A");
         author1.getAddress().setState("Delhi");
-        Subjects subjects = new Subjects();
-        subjects.setSubjectName("English");
-        /*subjects.setSubjectName("history");*/
-        author1.getListofSubjects().add(subjects);
-        session.save(subjects);
-        session.save(author1);
+        Subjects firstsubject = new Subjects();
+        firstsubject.setSubjectName("English");
+        Subjects secondsubject = new Subjects();
+        secondsubject.setSubjectName("History");
+        Subjects thirdsubject = new Subjects();
+        thirdsubject.setSubjectName("IOT");
+        author1.getListofSubjects().add(firstsubject);
+        author1.getListofSubjects().add(secondsubject);
+        author1.getListofSubjects().add(thirdsubject);
+        session.persist(firstsubject);
+        session.persist(secondsubject);
+        session.persist(thirdsubject);
+        session.persist(author1);
 
 
 
@@ -47,12 +54,19 @@ public class CreateAuthor {
         author2.getAddress().setLocation("Shalimar Bagh");
         author2.getAddress().setStreenNo("10");
         author2.getAddress().setState("noida");
-        Subjects subjects2 = new Subjects();
-        subjects2.setSubjectName("Computer Science");
-        /*subjects2.setSubjectName("Hibernate");*/
-        author2.getListofSubjects().add(subjects2);
-        session.save(subjects2);
-        session.save(author2);
+        Subjects firstsubject1 = new Subjects();
+        firstsubject1.setSubjectName("OOAD");
+        Subjects secondsubject1 = new Subjects();
+        secondsubject1.setSubjectName("Civics");
+        Subjects thirdsubject1 = new Subjects();
+        thirdsubject1.setSubjectName("Economics");
+        author2.getListofSubjects().add(firstsubject1);
+        author2.getListofSubjects().add(secondsubject1);
+        author2.getListofSubjects().add(thirdsubject1);
+        session.persist(firstsubject1);
+        session.persist(secondsubject1);
+        session.persist(thirdsubject1);
+        session.persist(author2);
 
 
 
@@ -67,12 +81,19 @@ public class CreateAuthor {
         author3.getAddress().setLocation("wales");
         author3.getAddress().setStreenNo("1A/g8");
         author3.getAddress().setState("Canada");
-        Subjects subjects3 = new Subjects();
-        subjects3.setSubjectName(" Theory of Computation");
-        /*subjects3.setSubjectName("Web TEch");*/
-        author1.getListofSubjects().add(subjects3);
-        session.save(subjects3);
-        session.save(author3);
+        Subjects firstsubject3 = new Subjects();
+        firstsubject3.setSubjectName("WEB-TECH");
+        Subjects secondsubject3 = new Subjects();
+        secondsubject3.setSubjectName("JAVA");
+        Subjects thirdsubject3 = new Subjects();
+        thirdsubject3.setSubjectName("C++");
+        author3.getListofSubjects().add(firstsubject3);
+        author3.getListofSubjects().add(secondsubject3);
+        author3.getListofSubjects().add(thirdsubject3);
+        session.persist(firstsubject3);
+        session.persist(secondsubject3);
+        session.persist(thirdsubject3);
+        session.persist(author3);
 
         Author author4 = new Author();
         author4.setFirstName("shubham");
@@ -85,13 +106,19 @@ public class CreateAuthor {
         author4.getAddress().setLocation("baba dham");
         author4.getAddress().setStreenNo("25");
         author4.getAddress().setState("bholenath gali");
-        Subjects subjects4 = new Subjects();
-        subjects4.setSubjectName("Computer System Architecture");
-        /*subjects4.setSubjectName(" Spring");
-        subjects4.setSubjectName(" Colllections");*/
-        author4.getListofSubjects().add(subjects4);
-        session.save(subjects4);
-        session.save(author4);
+        Subjects firstsubject4 = new Subjects();
+        firstsubject4.setSubjectName("FRENCH");
+        Subjects secondsubject4 = new Subjects();
+        secondsubject4.setSubjectName("GERMAN");
+        Subjects thirdsubject4 = new Subjects();
+        thirdsubject4.setSubjectName("POLitical");
+        author4.getListofSubjects().add(firstsubject4);
+        author4.getListofSubjects().add(secondsubject4);
+        author4.getListofSubjects().add(thirdsubject4);
+        session.persist(firstsubject4);
+        session.persist(secondsubject4);
+        session.persist(thirdsubject4);
+        session.persist(author4);
 
 
         session.getTransaction().commit();
